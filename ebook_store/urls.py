b3e490482184f94036188f795276bcfc657d7486
Danial Rafique <danialrafique7@gmail.com>
2024-08-25 20:46:08 -0700

    add url of staticfiles

--- ebook_store/urls.py
+++ ebook_store/urls.py
@@ -19,6 +19,7 @@ from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -50,3 +51,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
